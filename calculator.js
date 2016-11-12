852de3671da9742390757aac4ffc7ac7a54a518b
David Ebinger <davidebinger888@gmail.com>
2016-11-11 22:26:23 -1000

    Fixed #1 js-calculator

--- calculator.js
+++ calculator.js
@@ -15,108 +15,108 @@ function calculatorModule() {
    * @param  { Number } x
    * @return { Number }    current total
    */
-function load(x) {
-    if (typeof x === 'number'){
-     _total = x;
-      return _total;
-   }else{
-     throw new Error();
-   }
-}
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
-function getTotal(x) {
-  return _total;
-}
+    function _load(x) {
+        if (typeof x === 'number'){
+         _total = x;
+         return _total;
+       }else{
+         throw new Error();
+       }
+    }
+      /**
+       * Return the value of `total`
+       * @return { Number }
+       */
+    function _getTotal(x) {
+         return _total;
+    }
 
-  /**
-   * Sums the value passed in with `total`
-   * @param { Number } x
-   */
-function add(x) {
-  if (typeof x === 'number'){
-  _total += x;
-  return _total;
-  }else{
-    throw new Error();
-  }
-}
+      /**
+       * Sums the value passed in with `total`
+       * @param { Number } x
+       */
+    function _add(x) {
+        if (typeof x === 'number'){
+          _total += x;
+          return _total;
+        }else{
+          throw new Error();
+        }
+    }
 
-  /**
-   * Subtracts the value passed in from `total`
-   * @param  { Number } x
-   */
-function subtract(x) {
-  if (typeof x === 'number'){
-  _total -= x;
-  return _total;
-  }else{
-    throw new Error();
-  }
-}
+      /**
+       * Subtracts the value passed in from `total`
+       * @param  { Number } x
+       */
+    function _subtract(x) {
+        if (typeof x === 'number'){
+          _total -= x;
+          return _total;
+        }else{
+          throw new Error();
+        }
+    }
 
-  /**
-   * Multiplies the value by `total`
-   * @param  { Number } x
-   */
-function multiply(x) {
-    if (typeof x === 'number'){
-  _total *= x;
-  return _total;
-  }else{
-    throw new Error();
-  }
-}
+      /**
+       * Multiplies the value by `total`
+       * @param  { Number } x
+       */
+    function _multiply(x) {
+        if (typeof x === 'number'){
+          _total *= x;
+          return _total;
+        }else{
+          throw new Error();
+        }
+    }
 
-  /**
-   * Divides the value passing in by `total`
-   * @param  { Number } x
-   */
+      /**
+       * Divides the value passing in by `total`
+       * @param  { Number } x
+       */
 
-function divide(x) {
-    if (typeof x === 'number'){
-  _total /= x;
-  return _total;
-  }else{
-    throw new Error();
-  }
-}
-  /**
-   * Return the value stored at `memory`
-   * @return { Number }
-   */
-function recallMemory() {
-  return _memory;
-}
+    function _divide(x) {
+        if (typeof x === 'number'){
+          _total /= x;
+          return _total;
+        }else{
+          throw new Error();
+        }
+    }
+      /**
+       * Return the value stored at `memory`
+       * @return { Number }
+       */
+    function _recallMemory() {
+          return _memory;
+    }
 
-  /**
-   * Stores the value of `total` to `memory`
-   */
-function saveMemory() {
-  _memory = _total;
-}
+      /**
+       * Stores the value of `total` to `memory`
+       */
+    function _saveMemory() {
+          _memory = _total;
+    }
 
-  /**
-   * Clear the value stored at `memory`
-   */
-function clearMemory() {
-  _memory = 0;
-}
-  /**
-   * Validation
-   */
-function Validation(){}
-  return {
-    load: load,
-    getTotal: getTotal,
-    add: add,
-    subtract: subtract,
-    multiply: multiply,
-    divide: divide,
-    recallMemory: recallMemory,
-    saveMemory: saveMemory,
-    clearMemory: clearMemory
-  };
+      /**
+       * Clear the value stored at `memory`
+       */
+    function _clearMemory() {
+          _memory = 0;
+    }
+      /**
+       * Validation
+       */
+    function _Validation(){}
+        return {
+          load: _load,
+          getTotal: _getTotal,
+          add: _add,
+          subtract: _subtract,
+          multiply: _multiply,
+          divide: _divide,
+          recallMemory: _recallMemory,
+          saveMemory: _saveMemory,
+          clearMemory: _clearMemory
+        };
  }
